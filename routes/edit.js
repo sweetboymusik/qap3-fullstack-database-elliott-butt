9303a3ec5709e6b3f27051e736bb0f43cace6a67
Elliott <ellbutt@gmail.com>
2024-07-21 15:47:31 -0230

    set up edit route, added lodash package

--- routes/edit.js
+++ routes/edit.js
@@ -1,7 +1,11 @@
+// import express and set up router
 const express = require("express");
 const router = express.Router();
 
-// /edit
+// import DAL functions
+const { getItemByName, getItemById } = require("../services/items.dal");
+
+// base route (/edit)
 router.get("/", async (req, res) => {
   try {
     res.render("edit");
@@ -9,5 +13,33 @@ router.get("/", async (req, res) => {
     res.render("503");
   }
 });
+
+router.post("/", async (req, res) => {
+  // get inputted id or name and go to that /page
+});
+
+// edit item by name (/name/:name)
+router.get("/name/:name", async (req, res) => {
+  try {
+    const item = await getItemByName(req.params.name);
+    res.render("item", { item });
+  } catch (error) {
+    res.render("503");
+  }
+});
+
+// edit item by id (/id/:id)
+router.get("/id/:id", async (req, res) => {
+  try {
+    const item = await getItemById(req.params.id);
+    res.render("item", { item });
+  } catch (error) {
+    res.render("503");
+  }
+});
+
+// patch item
+
+// delete item
 
 module.exports = router;
--- services/items.dal.js
+++ services/items.dal.js
@@ -1,4 +1,5 @@
 const dal = require("./db");
+const _ = require("lodash");
 
 function getAllItems() {
   return new Promise(function (resolve, reject) {
@@ -28,7 +29,38 @@ function getItemsByCategory(category) {
   });
 }
 
+function getItemByName(name) {
+  return new Promise(function (resolve, reject) {
+    const sql = "SELECT * from items WHERE item_name = $1";
+    const nameCleaned = _.startCase(name);
+
+    dal.query(sql, [nameCleaned], (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result.rows);
+      }
+    });
+  });
+}
+
+function getItemById(id) {
+  return new Promise(function (resolve, reject) {
+    const sql = "SELECT * from items WHERE item_id = $1";
+
+    dal.query(sql, [id], (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result.rows);
+      }
+    });
+  });
+}
+
 module.exports = {
   getAllItems,
   getItemsByCategory,
+  getItemByName,
+  getItemById,
 };
